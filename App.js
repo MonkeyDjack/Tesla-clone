dacde5f3363240677e02264285fda797bf3752f9
Muhitdin Yahya <monkeydjack0@mail.ru>
2021-09-06 11:39:22 +0200

    car list and scroll effect

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CarItem from './components/CarItems';
-
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <CarItem name={"Model X"} 
-        tagline={"Order Online For"}
-        taglineCTA={"Touchless Delivery"}
-        image={require('./assets/images/ModelX.jpeg')} />
-
-
+        <CarsList />
     </View>
   );
 }
---
+++ components/CarsList/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, FlatList, Dimensions } from "react-native";
+import CarItem from "../CarItems";
+import cars from "./cars";
+import styles from "./styles";
+
+
+
+const CarsList = props =>{
+    return(
+        <View style={styles.container}>
+            <FlatList
+                data={cars}
+                renderItem={({item})=> <CarItem car={item} />}
+                snapToAlignment={'start'}
+                decelerationRate={'fast'}
+                snapToInterval={Dimensions.get('window').height}
+                showsVerticalScrollIndicator={'false'}
+            />
+        </View>
+    );
+};
+
+export default CarsList;
